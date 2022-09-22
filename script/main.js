484e7a9230851da7e0d8b14d7871b32b4d271179
hyewoon <likethebegin@naver.com>
2022-09-22 12:59:26 +0900

    css detail
    css detail onging

--- script/main.js
+++ script/main.js
@@ -1,1 +1,8 @@
-/$(function)(){}
+$(function(){
+		$("#detail").click(function(){
+			$(".item-detail").removeClass("slow");
+		})
+		$("#close").click(function(){
+			$(".item-detail").addClass("fast");
+		})
+})
