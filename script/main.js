ee2dfa867663e57038a0e52cc70dfa0b0142227d
hyewoon <likethebegin@naver.com>
2022-09-23 12:50:33 +0900

    javascript add
    popup show . hide, attribute get set

--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
 $(function(){
 		$("#detail").click(function(){
-			$(".item-detail").removeClass("slow");
+		//	$(".item-detail").removeClass("slow");
+			$(".item-detail").removeClass("d-none");
 		})
 		$("#close").click(function(){
-			$(".item-detail").addClass("fast");
+		//	$(".item-detail").addClass("fast");
+			$(".item-detail").addClass("d-none");
 		})
 })
