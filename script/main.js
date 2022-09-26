5c9efb74d8f0641ca2bc47ca166f37ee45aefcce
hyewoon <likethebegin@naver.com>
2022-09-26 11:26:21 +0900

    add sidePanel
    adding sidePanel finished

--- script/main.js
+++ script/main.js
@@ -7,4 +7,19 @@ $(function(){
 		//	$(".item-detail").addClass("fast");
 			$(".item-detail").addClass("d-none");
 		})
+			/*const toggleSidePanel = document.querySelector("#closePanel");
+		 	const SidePanel = document.querySelector("#sidePanel");
+		
+		 	
+		 	toggleSidePanel.$("click", function (){
+		 		if(SidePanel.$("collapsed")){
+		 		 console.log("있음")
+		 			SidePanel.classList.remove("collapsed")
+		 		}else {
+		 		 console.log("없음")
+		 			SidePanel.classList.add("collapsed")
+		 		 	
+		 		}
+		 		//SidePanel.classList.remove("collapsed")
+		 	}) */
 })
